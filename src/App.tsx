9058732590c8f0d7dc133b16673449d6fa31d8be
Sahan Chathura <73782580+SanthaKudu47@users.noreply.github.com>
2024-08-22 00:06:32 +0900

    refactor:fixed bugs on small screens

--- src/App.tsx
+++ src/App.tsx
@@ -14,8 +14,8 @@ function App() {
       <Colors />
       <FullStory />
       <P17ProChip />
-      <Camera />
-      <div className="bg-green-950 h-[2500px] flex w-full"></div>
+      {/* <Camera /> */}
+      {/* <div className="bg-green-950 h-[2500px] flex w-full"></div> */}
     </>
   );
 }
